2d89c5cc70e4f4f9e38d4d982c7f16ec65678379
launchpad5682 <launchpad5682@gmail.com>
2022-03-18 23:57:45 +0530

    feat: Data provider added with initial data loading for categories and videos

--- src/context/data-context.js
+++ src/context/data-context.js
@@ -1,11 +1,48 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useReducer } from "react";
 import { createContext, useContext } from "react";
+import { reducer } from "../reducer/reducer";
 
 export const DataContext = createContext();
 export const useDataProvider = () => useContext(DataContext);
 
+const initialState = {
+  videos: [],
+  categories: [],
+};
+
 export const DataProvider = ({ children }) => {
-  const value = { catgories: [] };
+  const [{ videos, categories }, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await axios.get("/api/videos");
+        // console.log(response);
+        const { data, status } = response;
+        if (status === 200) {
+          dispatch({ type: "SET_VIDEOS", payload: data.videos });
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+
+    (async () => {
+      try {
+        const response = await axios.get("/api/categories");
+        // console.log(response);
+        const { data, status } = response;
+        if (status === 200) {
+          dispatch({ type: "SET_CATEGORIES", payload: data.categories });
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, []);
+
+  const value = { videos, categories };
 
   return <DataContext.Provider value={value}>{children}</DataContext.Provider>;
 };
