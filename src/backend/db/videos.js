f7b8e4d8cb708e4d1ab02f7645ac2dc1ddefd6b9
launchpad5682 <launchpad5682@gmail.com>
2022-03-18 18:18:03 +0530

    feat: videos added in db

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -5,17 +5,91 @@
 
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
+    _id: "4qJFAX5IGTc",
+    title: "Ferb Fletcher and the Power of Stoicism",
+    creator: "Made2Express",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+      "Ferb Fletcher and the Power of Stoicism - Inspired by the popular Disney show, Phineas and Ferb.",
+    categoryName: "philosophy",
   },
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
+    _id: "m23J6Xz93RY",
+    title: "How SpongeBob Reveals The Secret to a Meaningful Life",
+    creator: "Made2Express",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      "SpongeBob Squarepants is a very optimistic character, and because of that, he attracts positive experiences into his life. In this video, I take a look at the episode Idiot Box to see what positive messages we can get from it. ",
+    categoryName: "philosophy",
+  },
+  {
+    _id: "TCjcYYSVyXA",
+    title: "",
+    creator: "Made2Express",
+    description:
+      "Nihilism and Depression, as Portrayed in South Park Songs from Minecraft Even a Rose Has Thorns - Sonic Lost World",
+    categoryName: "philosophy",
+  },
+  {
+    _id: "K8zAbdYx9SU",
+    title: "Geography Now! Russia",
+    creator: "Geography Now",
+    description:
+      "Welcome to Geography Now! This is the first and only Youtube Channel that actively attempts to cover profiles on every single country of the world. We are going to do them alphabetically so be patient if you are waiting for one that's down the road.",
+    categoryName: "geography",
+  },
+  {
+    _id: "wuClZjOdT30",
+    title: "Geography Now! Germany",
+    creator: "Geography Now",
+    description:
+      "Welcome to Geography Now! This is the first and only Youtube Channel that actively attempts to cover profiles on every single country of the world. We are going to do them alphabetically so be patient if you are waiting for one that's down the road.",
+    categoryName: "geography",
+  },
+  {
+    _id: "zqllxbPWKNI",
+    title: "The Napoleonic Wars - OverSimplified (Part 1)",
+    creator: "Oversimplified",
+    description:
+      "Thank you to our Patreon Presidents - Alexandra Scoma, Attila Oláh, Bobby Dellinger, Cara Akame, Chad Butler, Charlie Lichterman, cjwhitt89801, Danny Sprangemeijer, David Blackman, Elliot Lepley, Erik Halverson, Fernando López Ojeda, Glenn hEADcRASH Sugden, John, John Boyle, Kenneth Lowrey, Mark Davenport, Nickolas Ashlock, Nico Pita, Robert Riley, Sam Ingbar, Stacey Kadzewick, Susan Bryan, William, Yan Shen and Yubo Zhang",
+    categoryName: "history",
+  },
+  {
+    _id: "mY3SEMTROas",
+    title: "The Napoleonic Wars - OverSimplified (Part 2)",
+    creator: "Oversimplified",
+    description:
+      "Thank you to our Patreon Presidents - Alexandra Scoma, Attila Oláh, Bobby Dellinger, Cara Akame, Chad Butler, Charlie Lichterman, cjwhitt89801, Danny Sprangemeijer, David Blackman, Elliot Lepley, Erik Halverson, Fernando López Ojeda, Glenn hEADcRASH Sugden, John, John Boyle, Kenneth Lowrey, Mark Davenport, Nickolas Ashlock, Nico Pita, Robert Riley, Sam Ingbar, Stacey Kadzewick, Susan Bryan, William, Yan Shen and Yubo Zhang",
+    categoryName: "history",
+  },
+  {
+    _id: "Cln0J87vulU",
+    title: "The War of the Bucket-Oversimplified",
+    creator: "Oversimplified",
+    description:
+      "Copyright disclaimer - We do not give anyone permission to translate and/or reupload our videos or designs on YouTube or other social media platforms.",
+    categoryName: "history",
+  },
+  {
+    _id: "Jl3K63Rbygw",
+    title: "The War that changed the English Language - Mini Wars",
+    creator: "Oversimplified",
+    description:
+      "Copyright disclaimer - We do not give anyone permission to translate and/or reupload our videos or designs on YouTube or other social media platforms.",
+    categoryName: "history",
+  },
+  {
+    _id: "VB_GWz25B3Q",
+    title: "What if the World turned to Gold? - The Gold Apocalypse",
+    creator: "Kurzgesagt – In a Nutshell",
+    description:
+      "Let us explore the scientific mystery of what would happen to you, if Earth suddenly turned into gold! The “Midaspocalypse”, based on the ancient tale of King Midas who was cursed so everything he touched turned into gold. ",
+    categoryName: "evolution",
+  },
+  {
+    _id: "gLZJlf5rHVs",
+    title: "What If Earth got Kicked Out of the Solar System? Rogue Earth",
+    creator: "Kurzgesagt – In a Nutshell",
+    description:
+      "The night sky seems peaceful and orderly. But in reality, stars are careening through the galaxy at speeds of hundreds of thousands of kilometres per hour. Not bound by static formations but changing neighbourhoods constantly. Fortunately space is big, and so the stars of the Milky Way are very unlikely to hit us. Unfortunately, they don’t have to hit anything to make us have a really bad time on earth. And there are already stars starting to get very close.",
+    categoryName: "evolution",
   },
 ];
