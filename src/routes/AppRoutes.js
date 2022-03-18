ade2c847723135d37553b27176bd62eae378205b
launchpad5682 <launchpad5682@gmail.com>
2022-03-18 23:51:04 +0530

    feat: SingleVideo Page added

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,14 +1,20 @@
+import MockmanEs from "mockman-js";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import App from "../App";
 import { Home } from "./Home/Home";
+import { SingleVideo } from "./SingleVideo/SingleVideo";
+import { VideoListing } from "./VideoLisitng/VideoListing";
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<App />}>
         <Route index element={<Home />} />
+        <Route path="videos" element={<VideoListing />} />
+        <Route path="videos/:videoid" element={<SingleVideo />} />
       </Route>
+      <Route path="/api/mockman" element={<MockmanEs />} />
     </Routes>
   );
 }
