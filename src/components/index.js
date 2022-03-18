980f85266daebf40f1faab63fab7cf50a54dd978
launchpad5682 <launchpad5682@gmail.com>
2022-03-18 23:54:27 +0530

    feat: Added some components

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,5 @@
 export { Header } from "./Header/Header";
 export { HomeCategoryCard } from "./HomeCategoryCard/HomeCategoryCard";
+export { Sidebar } from "./Sidebar/Sidebar";
+export { VideoCard } from "./VideoCard/VideoCard";
+export { BadgeButton } from "./Buttons/BadgeButton";
