220a15e90ee2f9ad0da3f89188e070df6c261b8e
launchpad5682 <launchpad5682@gmail.com>
2022-03-21 01:58:52 +0530

    feat: Liked Videos, Playlist and Playlists page added

---
+++ src/routes/Playlists/Playlists.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { PlaylistCard } from "../../components/PlaylistCard/PlaylistCard";
+import { useAuthProvider } from "../../context/auth-context";
+import { useDataProvider } from "../../context/data-context";
+import { deletePlaylist } from "../../utils";
+
+export function Playlists() {
+  const { playlists, dispatch } = useDataProvider();
+  const { token } = useAuthProvider();
+
+  return (
+    <>
+      <div className="subheading">
+        <span className="h5__typography typography--white bold--typography">
+          Playlists {playlists.length}
+        </span>
+      </div>
+      {playlists.length > 0 ? (
+        <div className="flex--column padding--sm--vertical">
+          {playlists?.map((playlist) => (
+            <PlaylistCard
+              playlist={playlist}
+              key={playlist._id}
+              deleteHandler={() => deletePlaylist(playlist, token, dispatch)}
+            />
+          ))}
+        </div>
+      ) : (
+        <div className="h6__typography">No Playlists to show</div>
+      )}
+    </>
+  );
+}
