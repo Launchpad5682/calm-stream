1be082c7ced7c1e202d1b4d389b0a9fdab7ba42b
launchpad5682 <launchpad5682@gmail.com>
2022-03-21 01:59:49 +0530

    refactor: Video listing page updated

--- src/routes/VideoLisitng/VideoListing.jsx
+++ src/routes/VideoLisitng/VideoListing.jsx
@@ -1,36 +1,35 @@
 import React from "react";
-import { Sidebar, VideoCard } from "../../components";
+import { VideoCard } from "../../components";
+import { Modal } from "../../components/Modal/Modal";
 import { useDataProvider } from "../../context/data-context";
 import "./VideoListing.css";
 
 export function VideoListing() {
-  const { videos } = useDataProvider();
+  const { videos, modal } = useDataProvider();
 
   return (
-    <div className="main--section">
-      <Sidebar />
-      <main className="main--grid padding--sm">
-        <div className="subheading">
-          <span className="h5__typography typography--white bold--typography">
-            Videos {videos.length}
-          </span>
-          <button
-            className="button--sm button__nav button--red button__rounded--sm button__nav--black filter--btn"
-            id="open--filter"
-          >
-            <span className="button__typography typography--black">Filter</span>
-          </button>
+    <>
+      <div className="subheading">
+        <span className="h5__typography typography--white bold--typography">
+          Videos {videos.length}
+        </span>
+        <button
+          className="button--sm button__nav button--red button__rounded--sm button__nav--black filter--btn"
+          id="open--filter"
+        >
+          <span className="button__typography typography--black">Filter</span>
+        </button>
+      </div>
+      {videos.length > 0 ? (
+        <div className="grid-4-item padding--sm--vertical">
+          {videos?.map((video) => (
+            <VideoCard video={video} key={video._id} />
+          ))}
         </div>
-        {videos.length > 0 ? (
-          <div className="grid-4-item padding--sm--vertical">
-            {videos?.map((video) => (
-              <VideoCard video={video} key={video._id} />
-            ))}
-          </div>
-        ) : (
-          <div className="h6__typography">No videos to show</div>
-        )}
-      </main>
-    </div>
+      ) : (
+        <div className="h6__typography">No videos to show</div>
+      )}
+      {modal && <Modal />}
+    </>
   );
 }
