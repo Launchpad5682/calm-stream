d9b093b825b6d25ac6d9dd44aba7a42642df663a
launchpad5682 <launchpad5682@gmail.com>
2022-03-21 00:24:54 +0530

    feat: new actions like playlists, history, modal and drawer added to reducer

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -4,6 +4,18 @@ export const reducer = (state, action) => {
       return { ...state, categories: action.payload };
     case "SET_VIDEOS":
       return { ...state, videos: action.payload };
+    case "SET_LIKED_VIDEOS":
+      return { ...state, likes: action.payload };
+    case "SET_PLAYLISTS":
+      return { ...state, playlists: action.payload };
+    case "SET_HISTORY":
+      return { ...state, history: action.payload };
+    case "TOGGLE_MODAL":
+      return { ...state, modal: !state.modal };
+    case "SELECTED_VIDEO":
+      return { ...state, selectedVideo: action.payload };
+    case "TOGGLE_DRAWER":
+      return { ...state, drawerState: !state.drawerState };
     default:
       return state;
   }
