0b344b32cc595e66c51a8ca19ae2e4b16450128d
launchpad5682 <launchpad5682@gmail.com>
2022-03-21 01:01:21 +0530

    feat: Badge Button added

--- src/components/Buttons/BadgeButton.jsx
+++ src/components/Buttons/BadgeButton.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import "./BadgeButton.css";
 
-export function BadgeButton({ children }) {
-  return <span className="h5__typography typography--white">{children}</span>;
+export function BadgeButton({ children, active, clickHandler }) {
+  return (
+    <div className="badge--button">
+      <span
+        className={`h5__typography typography--${
+          active === true ? "green" : "white"
+        }`}
+        onClick={clickHandler}
+      >
+        {children}
+      </span>
+    </div>
+  );
 }
