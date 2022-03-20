a66028d8bf22771ee046e40adbd0f9704fbb17e1
launchpad5682 <launchpad5682@gmail.com>
2022-03-21 00:19:51 +0530

    feat: Private Route added

---
+++ src/helper/PrivateRoute.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { useAuthProvider } from "../context/auth-context";
+
+export function PrivateRoute({ children }) {
+  const { token } = useAuthProvider();
+
+  if (token !== null) {
+    return children;
+  }
+
+  return <Navigate to="/login" />;
+}
