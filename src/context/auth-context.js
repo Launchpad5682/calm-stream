f197d262e45559dde6fa4e7cf7f90777b03a6605
launchpad5682 <launchpad5682@gmail.com>
2022-03-21 00:29:54 +0530

    feat: Auth context added

--- src/context/auth-context.js
+++ src/context/auth-context.js
@@ -1,10 +1,42 @@
-import React from "react";
-import { createContext, useContext } from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { createContext } from "react";
+import { useContext } from "react";
 
 export const AuthContext = createContext();
 export const useAuthProvider = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
-  const value = {};
+  //   const cookieToken = JSON.parse();
+  const localStorageToken = JSON.parse(localStorage.getItem("login"));
+  const localStorageUser = JSON.parse(localStorage.getItem("user"));
+  const [user, setUser] = useState(localStorageUser?.user);
+  const [token, setToken] = useState(localStorageToken?.token);
+
+  const login = async (
+    email = "adarshbalika@gmail.com",
+    password = "adarshBalika123"
+  ) => {
+    try {
+      const response = await axios.post("/api/auth/login", {
+        email,
+        password,
+      });
+
+      if (response.status === 200) {
+        const {
+          data: { foundUser, encodedToken },
+        } = response;
+        setToken(encodedToken);
+        setUser(foundUser);
+        localStorage.setItem("login", JSON.stringify({ token: encodedToken }));
+        localStorage.setItem("user", JSON.stringify({ user: foundUser }));
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const value = { user, token, login };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
