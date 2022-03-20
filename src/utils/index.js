91e97b6aa3916fae4b857c4aa3ecd5f4dc73a7c6
launchpad5682 <launchpad5682@gmail.com>
2022-03-21 01:55:07 +0530

    feat: updated index file for utils

---
+++ src/utils/index.js
@@ -0,0 +1,5 @@
+export { openPlaylist } from "./handlePlaylists";
+export { deletePlaylist } from "./handlePlaylists";
+export { likeVideo } from "./handleLikeVideo";
+export { unLikeVideo } from "./handleLikeVideo";
+export { deleteVideoFromPlaylist } from "./handlePlaylists";
