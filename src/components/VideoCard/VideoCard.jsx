b320568c5ffda19b409d2fdb3ef5f0a13a9a5b13
launchpad5682 <launchpad5682@gmail.com>
2022-03-21 01:04:19 +0530

    feat: VideoCard card handler updated

--- src/components/VideoCard/VideoCard.jsx
+++ src/components/VideoCard/VideoCard.jsx
@@ -8,11 +8,21 @@ import {
 } from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
 import { BadgeButton } from "../Buttons/BadgeButton";
+import { useVideoLike } from "../../hooks/useVideoLike";
+import { useDataProvider } from "../../context/data-context";
+import { openPlaylist } from "../../utils";
 
 export function VideoCard({ video }) {
   const { _id, title, creator, categoryName, thumbnail } = video;
+ 
   const navigate = useNavigate();
-  const videoCardClickHandler = () => navigate(`/videos/${_id}`);
+
+  const videoCardClickHandler = () => {
+    navigate(`/videos/${_id}`);
+  };
+
+  const { likedVideo, likeHandler } = useVideoLike(video);
+  const { dispatch } = useDataProvider();
   return (
     <div className="card__flexcolumn card__flexcolumn--lg card__border--green video--card">
       <div className="item--image" onClick={videoCardClickHandler}>
@@ -25,18 +35,20 @@ export function VideoCard({ video }) {
         <span className="subtitle1__typography typography--white">
           {creator}
         </span>
-
         <div className="card--btns">
-          <BadgeButton>
+          <BadgeButton active={likedVideo} clickHandler={likeHandler}>
             <BsFillHandThumbsUpFill />
           </BadgeButton>
-          <BadgeButton>
+          <BadgeButton active={false} clickHandler={() => {}}>
             <BsFillHandThumbsDownFill />
           </BadgeButton>
-          <BadgeButton>
+          <BadgeButton
+            active={false}
+            clickHandler={() => openPlaylist(video, dispatch)}
+          >
             <BsFillCollectionPlayFill />
           </BadgeButton>
-          <BadgeButton>
+          <BadgeButton active={false} clickHandler={() => {}}>
             <BsStopwatchFill />
           </BadgeButton>
           <span className="subtitle1__typography typography--white category--tag h6__typography">
