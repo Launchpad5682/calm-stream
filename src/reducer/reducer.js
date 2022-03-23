4aa3902157fce503fe16a5c20622ee59518753fd
launchpad5682 <launchpad5682@gmail.com>
2022-03-23 23:03:25 +0530

    feat: reducer added

---
+++ src/reducer/reducer.js
@@ -0,0 +1,22 @@
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case "SET_CATEGORIES":
+      return { ...state, categories: action.payload };
+    case "SET_VIDEOS":
+      return { ...state, videos: action.payload };
+    case "SET_LIKED_VIDEOS":
+      return { ...state, likes: action.payload };
+    case "SET_PLAYLISTS":
+      return { ...state, playlists: action.payload };
+    case "SET_HISTORY":
+      return { ...state, history: action.payload };
+    case "TOGGLE_MODAL":
+      return { ...state, modal: !state.modal };
+    case "SELECTED_VIDEO":
+      return { ...state, selectedVideo: action.payload };
+    case "TOGGLE_DRAWER":
+      return { ...state, drawerState: !state.drawerState };
+    default:
+      return state;
+  }
+};
