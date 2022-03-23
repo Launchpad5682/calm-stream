71b91559c56a7d43fbde0d6255fe9c6677d4d3fb
launchpad5682 <launchpad5682@gmail.com>
2022-03-23 23:15:07 +0530

    feat: data-context added

--- src/context/data-context.js
+++ src/context/data-context.js
@@ -1,11 +1,76 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useReducer } from "react";
 import { createContext, useContext } from "react";
+import { reducer } from "../reducer/reducer"
 
 export const DataContext = createContext();
 export const useDataProvider = () => useContext(DataContext);
 
+const initialState = {
+  videos: [],
+  categories: [],
+  likes: [],
+  playlists: [],
+  history: [],
+  modal: false,
+  selectedVideo: null,
+  drawerState: false,
+};
+
 export const DataProvider = ({ children }) => {
-  const value = { catgories: [] };
+  const [
+    {
+      videos,
+      categories,
+      history,
+      likes,
+      modal,
+      selectedVideo,
+      playlists,
+      drawerState,
+    },
+    dispatch,
+  ] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await axios.get("/api/videos");
+        // console.log(response);
+        const { data, status } = response;
+        if (status === 200) {
+          dispatch({ type: "SET_VIDEOS", payload: data.videos });
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+
+    (async () => {
+      try {
+        const response = await axios.get("/api/categories");
+        // console.log(response);
+        const { data, status } = response;
+        if (status === 200) {
+          dispatch({ type: "SET_CATEGORIES", payload: data.categories });
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, []);
+
+  const value = {
+    videos,
+    categories,
+    likes,
+    history,
+    modal,
+    selectedVideo,
+    playlists,
+    drawerState,
+    dispatch,
+  };
 
   return <DataContext.Provider value={value}>{children}</DataContext.Provider>;
 };
