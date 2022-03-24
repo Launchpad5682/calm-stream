e3afc0b4d24dbb7d7d82bd9712e5848ddc7bdb95
launchpad5682 <launchpad5682@gmail.com>
2022-03-24 15:54:30 +0530

    feat: handle like video and watch later added

--- src/utils/handleWatchlater.js
+++ src/utils/handleWatchlater.js
@@ -1,29 +1,31 @@
 import axios from "axios";
 
-export const unLikeVideo = async (video, token, dispatch) => {
+export const removeFromWatchLater = async (video, token, dispatch) => {
   try {
-    const response = await axios.delete(`/api/user/likes/${video._id}`, {
+    const response = await axios.delete(`api/user/watchLater/${video._id}`, {
       headers: { authorization: token },
     });
     const { data, status } = response;
     if (status === 200) {
-      dispatch({ type: "SET_LIKED_VIDEOS", payload: data.likes });
+      dispatch({ type: "SET_WATCHLATER", payload: data.watchLater });
     }
   } catch (error) {
     console.error(error);
   }
 };
 
-export const likeVideo = async (video, token, dispatch) => {
+export const addToWatchLater = async (video, token, dispatch) => {
   try {
     const response = await axios.post(
-      "/api/user/likes",
-      { video: video },
+      "api/user/watchLater",
+      {
+        video: video,
+      },
       { headers: { authorization: token } }
     );
     const { data, status } = response;
     if (status === 201) {
-      dispatch({ type: "SET_LIKED_VIDEOS", payload: data.likes });
+      dispatch({ type: "SET_WATCHLATER", payload: data.watchLater });
     }
   } catch (error) {
     console.error(error);
