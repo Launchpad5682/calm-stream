a9de4903fb8064850c8f166f94daaaf7d304de8d
launchpad5682 <launchpad5682@gmail.com>
2022-03-24 15:22:57 +0530

    refactor: videos updated

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -12,7 +12,7 @@ export const videos = [
       "Ferb Fletcher and the Power of Stoicism - Inspired by the popular Disney show, Phineas and Ferb.",
     categoryName: "philosophy",
     thumbnail:
-      "https://i.ytimg.com/an_webp/4qJFAX5IGTc/mqdefault_6s.webp?du=3000&sqp=CO710ZEG&rs=AOn4CLBXQOYaCaDGbSvOXCNdfPahomazxw",
+      "https://yifgzyyqlpgydlzwcsaj.supabase.in/storage/v1/object/sign/calm-stream/ferb-fletcher-and-the-power-of-stoicism.webp?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJjYWxtLXN0cmVhbS9mZXJiLWZsZXRjaGVyLWFuZC10aGUtcG93ZXItb2Ytc3RvaWNpc20ud2VicCIsImlhdCI6MTY0Nzg4MTc5MSwiZXhwIjoxOTYzMjQxNzkxfQ.__lo3wgw1CCxqf_a8Wahx4zYy1jPOFwpQIWm3IWsfJA",
   },
   {
     _id: "m23J6Xz93RY",
@@ -22,7 +22,7 @@ export const videos = [
       "SpongeBob Squarepants is a very optimistic character, and because of that, he attracts positive experiences into his life. In this video, I take a look at the episode Idiot Box to see what positive messages we can get from it. ",
     categoryName: "philosophy",
     thumbnail:
-      "https://i.ytimg.com/an_webp/m23J6Xz93RY/mqdefault_6s.webp?du=3000&sqp=CNba0ZEG&rs=AOn4CLCmeb1Bo5kDOrKcOQDUOfccQ9eEkw",
+      "https://yifgzyyqlpgydlzwcsaj.supabase.in/storage/v1/object/sign/calm-stream/how-spongebob-reveals-the-secret-to-a-meaningful-life.webp?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJjYWxtLXN0cmVhbS9ob3ctc3BvbmdlYm9iLXJldmVhbHMtdGhlLXNlY3JldC10by1hLW1lYW5pbmdmdWwtbGlmZS53ZWJwIiwiaWF0IjoxNjQ3ODgxODYyLCJleHAiOjE5NjMyNDE4NjJ9.IzTK9UNJE5ExLCVrnTRnKRVuQ_Yd0_aUl1KOHINa-2s",
   },
   {
     _id: "TCjcYYSVyXA",
@@ -32,7 +32,7 @@ export const videos = [
       "Nihilism and Depression, as Portrayed in South Park Songs from Minecraft Even a Rose Has Thorns - Sonic Lost World",
     categoryName: "philosophy",
     thumbnail:
-      "https://i.ytimg.com/an_webp/TCjcYYSVyXA/mqdefault_6s.webp?du=3000&sqp=CMCD0pEG&rs=AOn4CLD_KniRmSCeYJjIXnBSKCRc2x56kg",
+      "https://yifgzyyqlpgydlzwcsaj.supabase.in/storage/v1/object/sign/calm-stream/nihilism-and-depression-as-portrayed-in-south-park.webp?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJjYWxtLXN0cmVhbS9uaWhpbGlzbS1hbmQtZGVwcmVzc2lvbi1hcy1wb3J0cmF5ZWQtaW4tc291dGgtcGFyay53ZWJwIiwiaWF0IjoxNjQ3ODgxODkyLCJleHAiOjE5NjMyNDE4OTJ9.68xIp0PGxm_uNXsaMR6tU28BseCfkEdmP0MGf3N0LDQ",
   },
   {
     _id: "K8zAbdYx9SU",
@@ -42,7 +42,7 @@ export const videos = [
       "Welcome to Geography Now! This is the first and only Youtube Channel that actively attempts to cover profiles on every single country of the world. We are going to do them alphabetically so be patient if you are waiting for one that's down the road.",
     categoryName: "geography",
     thumbnail:
-      "https://i.ytimg.com/an_webp/K8zAbdYx9SU/mqdefault_6s.webp?du=3000&sqp=CMv40ZEG&rs=AOn4CLCuX9naWo0Q7rQfOXG_LdXnwsYxuw",
+      "https://yifgzyyqlpgydlzwcsaj.supabase.in/storage/v1/object/sign/calm-stream/geography-now-russia.webp?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJjYWxtLXN0cmVhbS9nZW9ncmFwaHktbm93LXJ1c3NpYS53ZWJwIiwiaWF0IjoxNjQ3ODgxOTE2LCJleHAiOjE5NjMyNDE5MTZ9.ieXp7pENpTWbyPDJCA2AbqvLI_HfEFQ8iCyu9O_YeLY",
   },
   {
     _id: "wuClZjOdT30",
@@ -52,7 +52,7 @@ export const videos = [
       "Welcome to Geography Now! This is the first and only Youtube Channel that actively attempts to cover profiles on every single country of the world. We are going to do them alphabetically so be patient if you are waiting for one that's down the road.",
     categoryName: "geography",
     thumbnail:
-      "https://i.ytimg.com/an_webp/wuClZjOdT30/mqdefault_6s.webp?du=3000&sqp=CKKS0pEG&rs=AOn4CLDA18T-QbGCF07yqvAQhrOVD0fa6A",
+      "https://yifgzyyqlpgydlzwcsaj.supabase.in/storage/v1/object/sign/calm-stream/geography-now-germany.webp?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJjYWxtLXN0cmVhbS9nZW9ncmFwaHktbm93LWdlcm1hbnkud2VicCIsImlhdCI6MTY0Nzg4MTg0OSwiZXhwIjoxOTYzMjQxODQ5fQ.Xw0jTPCqUMAVSFuZm1clDHYSQ1C4ax8JAEzp-YuHOD0",
   },
   {
     _id: "zqllxbPWKNI",
@@ -62,7 +62,7 @@ export const videos = [
       "Thank you to our Patreon Presidents - Alexandra Scoma, Attila Oláh, Bobby Dellinger, Cara Akame, Chad Butler, Charlie Lichterman, cjwhitt89801, Danny Sprangemeijer, David Blackman, Elliot Lepley, Erik Halverson, Fernando López Ojeda, Glenn hEADcRASH Sugden, John, John Boyle, Kenneth Lowrey, Mark Davenport, Nickolas Ashlock, Nico Pita, Robert Riley, Sam Ingbar, Stacey Kadzewick, Susan Bryan, William, Yan Shen and Yubo Zhang",
     categoryName: "history",
     thumbnail:
-      "https://i.ytimg.com/an_webp/zqllxbPWKNI/mqdefault_6s.webp?du=3000&sqp=CNT50ZEG&rs=AOn4CLDKwI69RZIM7swrYrPqXhzfV1ZDJA",
+      "https://yifgzyyqlpgydlzwcsaj.supabase.in/storage/v1/object/sign/calm-stream/the-napoleonic-wars-part-1.webp?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJjYWxtLXN0cmVhbS90aGUtbmFwb2xlb25pYy13YXJzLXBhcnQtMS53ZWJwIiwiaWF0IjoxNjQ3ODgxOTg4LCJleHAiOjE5NjMyNDE5ODh9.-2xx28Yof5tYWOkNwVLXkQ9LxEoxHFYLxiIrU0PLOGc",
   },
   {
     _id: "mY3SEMTROas",
@@ -72,7 +72,7 @@ export const videos = [
       "Thank you to our Patreon Presidents - Alexandra Scoma, Attila Oláh, Bobby Dellinger, Cara Akame, Chad Butler, Charlie Lichterman, cjwhitt89801, Danny Sprangemeijer, David Blackman, Elliot Lepley, Erik Halverson, Fernando López Ojeda, Glenn hEADcRASH Sugden, John, John Boyle, Kenneth Lowrey, Mark Davenport, Nickolas Ashlock, Nico Pita, Robert Riley, Sam Ingbar, Stacey Kadzewick, Susan Bryan, William, Yan Shen and Yubo Zhang",
     categoryName: "history",
     thumbnail:
-      "https://i.ytimg.com/an_webp/mY3SEMTROas/mqdefault_6s.webp?du=3000&sqp=CKyC0pEG&rs=AOn4CLB-Qf7hhFBd5Bm6q1MnglHOEDt9RQ",
+      "https://yifgzyyqlpgydlzwcsaj.supabase.in/storage/v1/object/sign/calm-stream/the-napoleonic-wars-part-2.webp?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJjYWxtLXN0cmVhbS90aGUtbmFwb2xlb25pYy13YXJzLXBhcnQtMi53ZWJwIiwiaWF0IjoxNjQ3ODgyMDAwLCJleHAiOjE5NjMyNDIwMDB9.D9s9gfVfB7ndHEuzxylKYhk_tj__hQJsEXZOYXelUk4",
   },
   {
     _id: "Cln0J87vulU",
@@ -82,7 +82,7 @@ export const videos = [
       "Copyright disclaimer - We do not give anyone permission to translate and/or reupload our videos or designs on YouTube or other social media platforms.",
     categoryName: "history",
     thumbnail:
-      "https://i.ytimg.com/an_webp/Cln0J87vulU/mqdefault_6s.webp?du=3000&sqp=CNCY0pEG&rs=AOn4CLCVmBCyq-8Y47K8OXz2NtJS2J1AXA",
+      "https://yifgzyyqlpgydlzwcsaj.supabase.in/storage/v1/object/sign/calm-stream/the-war-of-the-bucket.webp?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJjYWxtLXN0cmVhbS90aGUtd2FyLW9mLXRoZS1idWNrZXQud2VicCIsImlhdCI6MTY0Nzg4MjA0MCwiZXhwIjoxOTYzMjQyMDQwfQ.M3khR0gcYvFqVfMd8okK5V8aVzFxKHPhS3KBGm2Rc7g",
   },
   {
     _id: "Jl3K63Rbygw",
@@ -92,7 +92,7 @@ export const videos = [
       "Copyright disclaimer - We do not give anyone permission to translate and/or reupload our videos or designs on YouTube or other social media platforms.",
     categoryName: "history",
     thumbnail:
-      "https://i.ytimg.com/an_webp/Jl3K63Rbygw/mqdefault_6s.webp?du=3000&sqp=CKj60ZEG&rs=AOn4CLDmMDPfEtclQsnN3mWI6s1JZjjCNw",
+      "https://yifgzyyqlpgydlzwcsaj.supabase.in/storage/v1/object/sign/calm-stream/the-war-changed-the-english-language.webp?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJjYWxtLXN0cmVhbS90aGUtd2FyLWNoYW5nZWQtdGhlLWVuZ2xpc2gtbGFuZ3VhZ2Uud2VicCIsImlhdCI6MTY0Nzg4MjAyNCwiZXhwIjoxOTYzMjQyMDI0fQ.W2e3gnV4Fry7Xb51TqpeEqR9sGiUmu84BVPRcO3ykHc",
   },
   {
     _id: "VB_GWz25B3Q",
@@ -102,7 +102,7 @@ export const videos = [
       "Let us explore the scientific mystery of what would happen to you, if Earth suddenly turned into gold! The “Midaspocalypse”, based on the ancient tale of King Midas who was cursed so everything he touched turned into gold. ",
     categoryName: "evolution",
     thumbnail:
-      "https://i.ytimg.com/an_webp/VB_GWz25B3Q/mqdefault_6s.webp?du=3000&sqp=CKDy0ZEG&rs=AOn4CLCMWKb6F5x4QoeNMWjQ7qKuogY_zg",
+      "https://yifgzyyqlpgydlzwcsaj.supabase.in/storage/v1/object/sign/calm-stream/what-if-the-world-turned-to-gold.webp?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJjYWxtLXN0cmVhbS93aGF0LWlmLXRoZS13b3JsZC10dXJuZWQtdG8tZ29sZC53ZWJwIiwiaWF0IjoxNjQ3ODgyMTI3LCJleHAiOjE5NjMyNDIxMjd9.nppSH2jtgAM30U3BrPUfEQeJWkYjucJRE8WoC4dmHM0",
   },
   {
     _id: "gLZJlf5rHVs",
@@ -112,6 +112,6 @@ export const videos = [
       "The night sky seems peaceful and orderly. But in reality, stars are careening through the galaxy at speeds of hundreds of thousands of kilometres per hour. Not bound by static formations but changing neighbourhoods constantly. Fortunately space is big, and so the stars of the Milky Way are very unlikely to hit us. Unfortunately, they don’t have to hit anything to make us have a really bad time on earth. And there are already stars starting to get very close.",
     categoryName: "evolution",
     thumbnail:
-      "https://i.ytimg.com/an_webp/gLZJlf5rHVs/mqdefault_6s.webp?du=3000&sqp=CJWC0pEG&rs=AOn4CLDlx3n4wQDiULhLbDBgPiYJTJ2S4Q",
+      "https://yifgzyyqlpgydlzwcsaj.supabase.in/storage/v1/object/sign/calm-stream/what-if-earth-got-kicked-out-of-the-solar-system.webp?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJjYWxtLXN0cmVhbS93aGF0LWlmLWVhcnRoLWdvdC1raWNrZWQtb3V0LW9mLXRoZS1zb2xhci1zeXN0ZW0ud2VicCIsImlhdCI6MTY0Nzg4MjA4NCwiZXhwIjoxOTYzMjQyMDg0fQ.vBb1L2Yw4FgMrjfLuV11TYxLGhEKIYK2OKKVOzj1_HU",
   },
 ];
