b9e821b9a22e24f1d4b308522a4961bbe9562ac5
launchpad5682 <launchpad5682@gmail.com>
2022-04-02 21:00:07 +0530

    feat: added alerts for video like

--- src/hooks/useVideoLike.js
+++ src/hooks/useVideoLike.js
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import { useAuthProvider } from "../context/auth-context";
 import { useDataProvider } from "../context/data-context";
-import { likeVideo, unLikeVideo } from "../utils";
+import { ACTION_TYPE, likeVideo, unLikeVideo } from "../utils";
+import { useAuth } from "./useAuth";
 
 export function useVideoLike(video) {
   const [likedVideo, setLikedVideo] = useState(false);
   const { likes, dispatch } = useDataProvider();
   const { token } = useAuthProvider();
+  const { isAuthenticated } = useAuth();
 
   useEffect(() => {
     if (likes.find((like) => like._id === video._id)) {
@@ -17,10 +19,25 @@ export function useVideoLike(video) {
   }, [likes, video]);
 
   const likeHandler = () => {
-    if (likedVideo) {
-      unLikeVideo(video, token, dispatch);
+    if (isAuthenticated) {
+      if (likedVideo) {
+        unLikeVideo(video, token, dispatch);
+        dispatch({
+          type: ACTION_TYPE.ACTIVATE_ALERT,
+          payload: { message: "Video removed from liked", color: "red" },
+        });
+      } else {
+        likeVideo(video, token, dispatch);
+        dispatch({
+          type: ACTION_TYPE.ACTIVATE_ALERT,
+          payload: { message: "Video added to liked", color: "green" },
+        });
+      }
     } else {
-      likeVideo(video, token, dispatch);
+      dispatch({
+        type: ACTION_TYPE.ACTIVATE_ALERT,
+        payload: { message: "You need to login", color: "green" },
+      });
     }
   };
 
