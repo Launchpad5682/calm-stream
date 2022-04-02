2591a0e1aa99ef51c2842749d34b39fd1be26a51
launchpad5682 <launchpad5682@gmail.com>
2022-04-02 17:19:28 +0530

    feat: Loader added

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Loader.css";
+
+export function Loader() {
+  return (
+    <div className="loader--container">
+      <div className="loader"></div>
+    </div>
+  );
+}
