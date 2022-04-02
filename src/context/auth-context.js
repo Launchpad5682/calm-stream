abaea0fc0ccf29440ef8a4edae7e4d89eb27718e
launchpad5682 <launchpad5682@gmail.com>
2022-04-02 21:27:20 +0530

    feat: Added alert for login status

--- src/context/auth-context.js
+++ src/context/auth-context.js
@@ -2,17 +2,19 @@ import axios from "axios";
 import React, { useState } from "react";
 import { createContext } from "react";
 import { useContext } from "react";
+import { ACTION_TYPE } from "../utils";
+import { useDataProvider } from "./data-context";
 
 export const AuthContext = createContext();
 export const useAuthProvider = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
-  //   const cookieToken = JSON.parse();
   const localStorageToken = JSON.parse(localStorage.getItem("login"));
   const localStorageUser = JSON.parse(localStorage.getItem("user"));
   const [user, setUser] = useState(localStorageUser?.user);
   const [token, setToken] = useState(localStorageToken?.token);
   const [loading, setLoading] = useState(false);
+  const { dispatch } = useDataProvider();
 
   const login = async (
     email = "adarshbalika@gmail.com",
@@ -34,10 +36,18 @@ export const AuthProvider = ({ children }) => {
         localStorage.setItem("login", JSON.stringify({ token: encodedToken }));
         localStorage.setItem("user", JSON.stringify({ user: foundUser }));
         setLoading(false);
+        dispatch({
+          type: ACTION_TYPE.ACTIVATE_ALERT,
+          payload: { message: "Logged In successful", color: "green" },
+        });
       }
     } catch (err) {
       console.error(err);
       setLoading(false);
+      dispatch({
+        type: ACTION_TYPE.ACTIVATE_ALERT,
+        payload: { message: "Something went wrong, try again", color: "red" },
+      });
     }
   };
 
