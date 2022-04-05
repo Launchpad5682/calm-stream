354a56a6537f9d1c8e588c4ad7b66d51b4eb924e
launchpad5682 <launchpad5682@gmail.com>
2022-04-05 12:55:39 +0530

    feat: Search Bar logic hook added

---
+++ src/components/SearchBar/useSearchBar.js
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useDataProvider } from "../../context/data-context";
+import { ACTION_TYPE } from "../../utils";
+
+export function useSearchBar() {
+  const [searchInput, setSearchInput] = useState("");
+  const { dispatch } = useDataProvider();
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+
+  const changeHandler = (event) => {
+    if (event.target.value === "")
+      dispatch({ type: ACTION_TYPE.SET_SEARCH, payload: event.target.value });
+    setSearchInput(event.target.value);
+  };
+
+  const searchHandler = (event) => {
+    if (event.key === "Enter") {
+      dispatch({ type: ACTION_TYPE.SET_SEARCH, payload: event.target.value });
+      dispatch({ type: ACTION_TYPE.TOGGLE_LOADING, payload: { videos: true } });
+      setTimeout(() => {
+        dispatch({
+          type: ACTION_TYPE.TOGGLE_LOADING,
+          payload: { videos: false },
+        });
+      }, 1000);
+      if (pathname !== "/videos") {
+        navigate("/videos");
+      }
+    }
+  };
+
+  const searchClickHandler = () => {
+    dispatch({ type: ACTION_TYPE.SET_SEARCH, payload: searchInput });
+    dispatch({ type: ACTION_TYPE.TOGGLE_LOADING, payload: { videos: true } });
+    setTimeout(() => {
+      dispatch({
+        type: ACTION_TYPE.TOGGLE_LOADING,
+        payload: { videos: false },
+      });
+    }, 1000);
+    if (pathname !== "/") {
+      navigate("/");
+    }
+  };
+
+  const clearHandler = () => {
+    dispatch({ type: ACTION_TYPE.SET_SEARCH, payload: "" });
+    setSearchInput("");
+  };
+
+  return {
+    searchInput,
+    changeHandler,
+    searchHandler,
+    clearHandler,
+    searchClickHandler,
+  };
+}
