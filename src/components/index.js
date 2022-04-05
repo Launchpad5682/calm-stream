13c0efdfdd8d8bf958358bb4cbb25d46f39fb691
launchpad5682 <launchpad5682@gmail.com>
2022-04-05 18:41:03 +0530

    refactor: updated components

--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,7 @@ export { CheckBoxInput } from "./Input/CheckBoxInput/CheckBoxInput";
 export { Header } from "./Header/Header";
 export { HomeCategoryCard } from "./HomeCategoryCard/HomeCategoryCard";
 export { PlaylistCard } from "./PlaylistCard/PlaylistCard";
+export { SolidButton } from "./Buttons/SolidButton";
 export { Sidebar } from "./Sidebar/Sidebar";
 export { VideoCard } from "./VideoCard/VideoCard";
 export { VerticalCard } from "./VerticalCard/VerticalCard";
