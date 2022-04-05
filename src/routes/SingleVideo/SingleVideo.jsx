42e2b4733b13cd8f10e9acde0898ae501196fb4b
launchpad5682 <launchpad5682@gmail.com>
2022-04-05 16:06:27 +0530

    feat: Single Video route added with loader

--- src/routes/SingleVideo/SingleVideo.jsx
+++ src/routes/SingleVideo/SingleVideo.jsx
@@ -15,6 +15,7 @@ import { ACTION_TYPE, openPlaylist } from "../../utils";
 import "./SingleVideo.css";
 import { useSingleVideo } from "./useSingleVideo";
 import { useNavigate } from "react-router-dom";
+import { Loader } from "../../components/Loader/Loader";
 
 export function SingleVideo() {
   const { video, likedVideo, likeHandler, inWatchLater, watchLaterHandler } =
@@ -52,52 +53,59 @@ export function SingleVideo() {
 
   return (
     <>
-      <div className="single__video--page">
-        <div className="single__video--container">
-          <span className="h6__typography" onClick={() => navigate(-2)}>
-            <BiArrowBack />
-          </span>
-          <div className="iframe--container">
-            <iframe
-              className="iframe--video"
-              src={`https://www.youtube.com/embed/${video._id}`}
-              title="YouTube video player"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            ></iframe>
-          </div>
-          <span className="typography--white h4__typography">
-            {video.title}
-          </span>
-          <div className="single__video--container">
-            <div className="typography--white h5__typography button--container">
-              <BadgeButton active={likedVideo} clickHandler={likeHandler}>
-                <BsFillHandThumbsUpFill />
-              </BadgeButton>
-              <BadgeButton active={false} clickHandler={() => {}}>
-                <BsFillHandThumbsDownFill />
-              </BadgeButton>
-              <BadgeButton
-                active={false}
-                clickHandler={() => openPlaylist(video, dispatch)}
-              >
-                <BsFillCollectionPlayFill />
-              </BadgeButton>
-              <BadgeButton
-                active={inWatchLater}
-                clickHandler={() => watchLaterHandler()}
-              >
-                <BsStopwatchFill />
-              </BadgeButton>
+      {Object.keys(video).length === 0 ? (
+        <Loader />
+      ) : (
+        <>
+          <div className="single__video--page">
+            <div className="single__video--container">
+              <span className="h6__typography" onClick={() => navigate(-1)}>
+                <BiArrowBack />
+              </span>
+              <div className="iframe--container">
+                <iframe
+                  className="iframe--video"
+                  src={`https://www.youtube.com/embed/${video._id}`}
+                  title="YouTube video player"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                  onPla
+                ></iframe>
+              </div>
+              <span className="typography--white h4__typography">
+                {video.title}
+              </span>
+              <div className="single__video--container">
+                <div className="typography--white h5__typography button--container">
+                  <BadgeButton active={likedVideo} clickHandler={likeHandler}>
+                    <BsFillHandThumbsUpFill />
+                  </BadgeButton>
+                  <BadgeButton active={false} clickHandler={() => {}}>
+                    <BsFillHandThumbsDownFill />
+                  </BadgeButton>
+                  <BadgeButton
+                    active={false}
+                    clickHandler={() => openPlaylist(video, dispatch)}
+                  >
+                    <BsFillCollectionPlayFill />
+                  </BadgeButton>
+                  <BadgeButton
+                    active={inWatchLater}
+                    clickHandler={() => watchLaterHandler()}
+                  >
+                    <BsStopwatchFill />
+                  </BadgeButton>
+                </div>
+                <p className="typography--white body1__typography paragraph">
+                  {video.description}
+                </p>
+                <div className="paragraph"></div>
+              </div>
             </div>
-            <p className="typography--white body1__typography paragraph">
-              {video.description}
-            </p>
-            <div className="paragraph"></div>
           </div>
-        </div>
-      </div>
-      {modal && <Modal />}
+          {modal && <Modal />}
+        </>
+      )}
     </>
   );
 }
