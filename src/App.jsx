a435d75d189093975ee4650d167c851ddb8cb373
launchpad5682 <launchpad5682@gmail.com>
2022-04-05 15:40:43 +0530

    feat: Removed home page with App Container as an Outlet

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+import { Outlet } from "react-router-dom";
+import { Header, Sidebar, Alert } from "./components";
+
+function App() {
+  return (
+    <div className="body--section">
+      <Header />
+      <div className="main--section">
+        <Sidebar />
+        <main className="main__section">
+          <Outlet />
+          <Alert />
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default App;
