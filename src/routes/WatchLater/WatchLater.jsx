a762e57312d2adb53d947adee999064f2aa24ba7
launchpad5682 <launchpad5682@gmail.com>
2022-08-13 15:54:09 +0530

    feat: Watch Later on load get videos added

--- src/routes/WatchLater/WatchLater.jsx
+++ src/routes/WatchLater/WatchLater.jsx
@@ -1,17 +1,41 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 import { VerticalCard } from "../../components";
 import { useAuthProvider } from "../../context/auth-context";
 import { useDataProvider } from "../../context/data-context";
-import { removeFromWatchLater } from "../../utils";
+import { ACTION_TYPE, removeFromWatchLater } from "../../utils";
 
 export function WatchLater() {
   const { watchlater, dispatch } = useDataProvider();
   const { token } = useAuthProvider();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.REACT_APP_API_URI}/watchLater/`,
+          {
+            headers: { authorization: token },
+          }
+        );
+        const { data, status } = response;
+        if (status === 200) {
+          dispatch({
+            type: ACTION_TYPE.SET_WATCHLATER,
+            payload: data.watchLater,
+          });
+        }
+      } catch (error) {
+        console.error(error, "Error from remove from watch later");
+      }
+    })();
+  }, [dispatch, token]);
+
   return (
     <>
       <div className="subheading">
         <span className="h5__typography typography--white bold--typography">
-          Watch Later {watchlater.length}
+          Watch Later {watchlater.length > 0 && watchlater.length}
         </span>
       </div>
       {watchlater.length > 0 ? (
